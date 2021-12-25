8764dd2b121f00c522b253dd79e08e46b2058322
JIPSlok <jonathanpoli17@gmail.com>
2021-12-25 13:04:59 -0300

    novas funções no gráfico de barras

--- Grafico_de_Barras/__init__.py
+++ Grafico_de_Barras/__init__.py
@@ -15,5 +15,12 @@ plt.title("Meus filmes favoritos")
 #Selo eixo y
 plt.ylabel("Número de Oscars")
 
+#nomeia o eixo x (>) com o nome dos filmes, 1 param: lista com os numeros de cada filme que tera, 2 param: lista com os nomes dos filmes
+plt.xticks([num for num, _ in enumerate(movies)], movies)
+
+#recebe uma lista com valores númericos, os 2 primeiros valores é o eixo x os 2 segundos é o eixo y
+#plt.axis([eixo x menor, eixo x maior, eixo y menor, eixo y maior])
+#Seja criterioso quando usar plt.axis(). Ao criar Graficos de barra, não começar o eixo y em 0 é considerado ruim, já que essa é uma maneira fácil de enganar as pessoas.
+
 #renderiza o grafico
 plt.show()
